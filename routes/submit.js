4753065cfd6b1ce60308b034c41caed31271e2cb
Brien Bledsoe <bab619@nyu.edu>
2019-12-11 17:18:02 -0500

    testing

--- routes/submit.js
+++ routes/submit.js
@@ -24,31 +24,7 @@ const firebaseConfig = {
  //set sample data to submit upon request. Make sure we have it connected and that we are sending data and
  //that it works
 
- const sampleData = {
-   title: 'Test',
-   test: 'Test Text',
-   author: 'Brien Bledsoe'
- }
-
-//Test Route
-router.get("/test", (req,res)=>{
-//this code is making sure that our process is working
-//the user is going to come to the node server but they are not posting any data
-//the get is just the user coming to the node server
-//were not posting to our server we are just getting data at this moment
-//the user simply wants to hit an endpoint and the node server is making a post request so we are creating a get
-//request to make this happend
-    try{
-      db.collection("Posts")
-      //setting an ID for the test doc
-      .doc("test-doc") //setting the doc name
-      .set(sampleData) //setting the doc to be our test-doc object
-      return res.send(`${sampleData} Test Data Submitted`)
-    }catch(e){
-      return res.send(`${e} There is an error`)
-    }
-
-})
+ 
 //submit data
 router.get("/", (req,res)=>{
   //need to get a URL parameter here in node, how do we do this?
